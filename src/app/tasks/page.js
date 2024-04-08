d15ec8c78011e8e94312bb1c85f854007edd9097
kokanek <kapeel.kokane@gmail.com>
2024-04-08 22:14:44 +0530

    First commit

---
+++ src/app/tasks/page.js
@@ -0,0 +1,30 @@
+import styles from "../page.module.css";
+
+export default function Tasks() {
+  return (
+    <main className={styles.main} >
+      <div className={styles.description}>
+        <p>
+          This is the&nbsp;
+          <code className={styles.code}>tasks</code>
+          &nbsp;page
+        </p>
+        <p>
+          This is the&nbsp;
+          <code className={styles.code}>tasks</code>
+          &nbsp;page
+        </p>
+        <p>
+          This is the&nbsp;
+          <code className={styles.code}>tasks</code>
+          &nbsp;page
+        </p>
+        <p>
+          This is the&nbsp;
+          <code className={styles.code}>tasks</code>
+          &nbsp;page
+        </p>
+      </div>
+    </main>
+  )
+}
