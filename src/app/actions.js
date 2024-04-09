9f16627f354103dbeaa0e645426a95319f9eff26
kokanek <kapeel.kokane@gmail.com>
2024-04-09 11:46:36 +0530

    unstable_cache with tag revalidation

--- src/app/actions.js
+++ src/app/actions.js
@@ -1,12 +1,12 @@
 'use server'
 import { kv } from '@vercel/kv';
-import { revalidatePath } from 'next/cache'
+import { revalidateTag } from 'next/cache'
 
 export async function editUser() {
   try {
     console.log('started editing the user')
-    await kv.hset('user:me', { id: 123, email: 'test@me.com' });
-    revalidatePath('/tasks')
+    await kv.hset('user:me', { id: 123, email: 'test3@me.com' });
+    revalidateTag('user')
     console.log('edited the user')
   } catch (error) {
     console.log('error while editing user')
--- src/app/tasks/page.js
+++ src/app/tasks/page.js
@@ -1,8 +1,19 @@
 import styles from "../page.module.css";
 import { kv } from '@vercel/kv';
+import { unstable_cache } from 'next/cache';
+
+export function getUser() {
+  return kv.hgetall('user:me');
+}
+
+const getCachedUser = unstable_cache(
+  async () => getUser(),
+  ['my-app-user'],
+  { tags: ['user'] }
+);
 
 export default async function Tasks() {
-  const user = await kv.hgetall('user:me');
+  const user = await getCachedUser();
   // const userJson = await NextResponse.json(user);
   console.log('user: ', user);
 
